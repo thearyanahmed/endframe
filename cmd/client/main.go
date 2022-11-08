a3d0a8f81c67cd1a043cc95c786e889f99cee343
Aryan Ahmed Anik <thearyanahmed@gmail.com>
2022-11-08 12:39:17 +0100

    Add client simulation

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -2,8 +2,27 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
 )
 
 func main() {
-	fmt.Print("client app")
+	start := time.Now()
+	ch := make(chan string)
+
+	for i := 0; i < 100; i++ {
+		go makeRequest(ch)
+	}
+	for i := 0; i < 100; i++ {
+		fmt.Println(<-ch)
+	}
+
+	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+}
+
+func makeRequest(ch chan<- string) {
+	resp, _ := http.Get("https://jsonplaceholder.typicode.com/posts")
+	body, _ := ioutil.ReadAll(resp.Body)
+	ch <- fmt.Sprintf(string(body))
 }
--- core/handler/router.go
+++ core/handler/router.go
@@ -2,8 +2,9 @@ package handler
 
 import (
 	"fmt"
-	"github.com/go-chi/chi/v5/middleware"
 	"net/http"
+
+	"github.com/go-chi/chi/v5/middleware"
 
 	"github.com/go-chi/chi/v5"
 )
@@ -13,13 +14,36 @@ func NewRouter() http.Handler {
 
 	r.Use(middleware.RequestID)
 	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
 
-	r.Get("/health-check", func(w http.ResponseWriter, r *http.Request) {
-		_, _ = w.Write([]byte("hello world"))
-	})
+	r.Route("/core/api", func(r chi.Router) {
+		r.Route("/v1", func(r chi.Router) {
+			// content type middleware?
 
-	r.Get("/test", func(w http.ResponseWriter, r *http.Request) {
-		_, _ = w.Write([]byte(fmt.Sprintf("request id: %s", middleware.GetReqID(r.Context()))))
+			// @todo Add client authorization token middleware. Have different tokens for different routes.
+			// use static tokens for simplicity
+
+			// Return health check status of this services and pings redis, kafka and db? (ping all related services)
+			// @q: ping user goroutines?
+			r.Get("/health-check", func(w http.ResponseWriter, r *http.Request) {
+				_, _ = w.Write([]byte("hello world"))
+			})
+
+			// This endpoint should return all available vehichles within an area
+			// also should support filter by query params
+			// optional: perhaps we can also filter by the radius
+			r.Get("/rides-near-by/{lat}/{long}/", func(w http.ResponseWriter, r *http.Request) {
+				_, _ = w.Write([]byte(fmt.Sprintf("lat: %s,long: %s", chi.URLParam(r, "lat"), chi.URLParam(r, "long"))))
+			})
+
+			// Update ride position
+			r.Post("/update/ride-position", func(w http.ResponseWriter, r *http.Request) {
+				w.Write([]byte("update ride position"))
+			})
+
+			
+
+		})
 	})
 
 	return r
