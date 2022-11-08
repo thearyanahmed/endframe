16caa7ca34755f63303f1df65b59b8b89c50b9b5
Aryan Ahmed Anik <thearyanahmed@gmail.com>
2022-11-08 18:53:09 +0100

    Add authorize client middleware

---
+++ core/middleware/authorize_client_middleware.go
@@ -0,0 +1,55 @@
+package middleware
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5/middleware"
+	log "github.com/sirupsen/logrus"
+	"github.com/thearyanahmed/nordsec/core/presenter"
+)
+
+type authorizeClientMiddleware struct {
+	authToken string
+	logger    log.Logger
+}
+
+func NewAuthorizeClientMiddleware(token string, logger log.Logger) *authorizeClientMiddleware {
+	return &authorizeClientMiddleware{
+		authToken: token,
+		logger:    logger,
+	}
+}
+
+func (m *authorizeClientMiddleware) Handle(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		token := r.Header.Get("Authorization")
+
+		if token == "" {
+			go func() {
+				m.logger.
+					WithField("req-id", middleware.GetReqID(r.Context())).
+					Error("attempt to authorize without auth token")
+			}()
+			presenter.ErrorResponse(w, r, presenter.ErrUnauthorized())
+			return
+		}
+
+		if token != m.authToken {
+			go func() {
+				m.logger.
+					WithField("req-id", middleware.GetReqID(r.Context())).
+					WithField("used-token", token).
+					Error("attempt to authorize without auth token")
+
+			}()
+			presenter.ErrorResponse(w, r, presenter.ErrUnauthorized())
+			return
+		}
+		go func() {
+			m.logger.
+				WithField("req-id", middleware.GetReqID(r.Context())).
+				Trace("authorized to client app")
+		}()
+		next.ServeHTTP(w, r)
+	})
+}
