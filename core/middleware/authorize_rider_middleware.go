d74bc59fb2cfc3e6c1e542616d49e2e640493236
Aryan Ahmed Anik <thearyanahmed@gmail.com>
2022-11-08 18:44:11 +0100

    Separate middleware from handler, add authorize rider middleware

---
+++ core/middleware/authorize_rider_middleware.go
@@ -0,0 +1,51 @@
+package middleware
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5/middleware"
+	log "github.com/sirupsen/logrus"
+	"github.com/thearyanahmed/nordsec/core/presenter"
+)
+
+type authorizeRiderMiddleware struct {
+	authToken string
+	logger    log.Logger
+}
+
+func NewAuthorizeRiderMiddleware(token string, logger log.Logger) *authorizeRiderMiddleware {
+	return &authorizeRiderMiddleware{
+		authToken: token,
+		logger:    logger,
+	}
+}
+
+func (m *authorizeRiderMiddleware) Handle(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		token := r.Header.Get("Authorization")
+
+		if token == "" {
+			go func() {
+				m.logger.
+					WithField("req-id", middleware.GetReqID(r.Context())).
+					Error("attempt to authorize without auth token")
+			}()
+			presenter.ErrorResponse(w, r, presenter.ErrUnauthorized())
+			return
+		}
+
+		if token != m.authToken {
+			go func() {
+				m.logger.
+					WithField("req-id", middleware.GetReqID(r.Context())).
+					WithField("used-token", token).
+					Error("attempt to authorize without auth token")
+
+			}()
+			presenter.ErrorResponse(w, r, presenter.ErrUnauthorized())
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
