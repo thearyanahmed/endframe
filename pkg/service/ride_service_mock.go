a31fe231310240df702762d2796cc23dfade639e
Aryan Ahmed Anik <thearyanahmed@gmail.com>
2022-11-11 19:11:21 +0100

    Add mock service

--- pkg/service/ride_service_mock.go
+++ pkg/service/ride_service_mock.go
@@ -1,1 +1,20 @@
 package service
+
+import (
+	"context"
+	"github.com/stretchr/testify/mock"
+	locationEntity "github.com/thearyanahmed/nordsec/services/location/entity"
+)
+
+type RideServiceMock struct {
+	mock.Mock
+}
+
+func (s *RideServiceMock) UpdateRideLocation(ctx context.Context, event locationEntity.Event) (locationEntity.Event, error) {
+	args := s.Called()
+	return args.Get(0).(locationEntity.Event), args.Error(1)
+}
+
+func (s *RideServiceMock) ResetMock() {
+	s.Mock = mock.Mock{}
+}
--- pkg/testutil/faker.go
+++ pkg/testutil/faker.go
@@ -1,1 +1,14 @@
 package testutil
+
+import (
+	"github.com/brianvoe/gofakeit/v6"
+	"github.com/thearyanahmed/nordsec/pkg/serializer"
+)
+
+func FakeRideEventRequest() serializer.RecordRideEventRequest {
+	return serializer.RecordRideEventRequest{
+		RideUuid:  gofakeit.UUID(),
+		Latitude:  gofakeit.Latitude(),
+		Longitude: gofakeit.Longitude(),
+	}
+}
