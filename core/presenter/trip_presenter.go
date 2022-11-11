93861c20a57ce642122a3a1200fab0113b4f6e36
Aryan Ahmed Anik <thearyanahmed@gmail.com>
2022-11-11 12:02:18 +0100

    Add presenter and request

--- core/presenter/trip_presenter.go
+++ core/presenter/trip_presenter.go
@@ -1,1 +1,20 @@
 package presenter
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func ErrDistanceTooLowResponse(minDistance float64) *Response {
+	return &Response{
+		HttpStatusCode: http.StatusBadRequest,
+		Message:        fmt.Sprintf("distance too low. minimum distance required %.2f meters or greater", minDistance),
+	}
+}
+
+func ErrRideUnavailableResponse() *Response {
+	return &Response{
+		HttpStatusCode: http.StatusUnprocessableEntity,
+		Message:        "ride is unavailable",
+	}
+}
--- core/serializer/start_trip_request.go
+++ core/serializer/start_trip_request.go
@@ -1,1 +1,40 @@
 package serializer
+
+import (
+	"github.com/thearyanahmed/nordsec/services/location"
+	"github.com/thedevsaddam/govalidator"
+)
+
+type StartTripRequest struct {
+	RideUuid             string  `json:"ride_uuid" schema:"ride_uuid"`
+	ClientUuid           string  `json:"client_uuid" schema:"client_uuid"`
+	OriginLatitude       float64 `json:"origin_latitude" schema:"origin_latitude"`
+	OriginLongitude      float64 `json:"origin_longitude" schema:"origin_longitude"`
+	DestinationLatitude  float64 `json:"destination_latitude" schema:"destination_latitude"`
+	DestinationLongitude float64 `json:"destination_longitude" schema:"destination_longitude"`
+}
+
+func (r *StartTripRequest) Rules() govalidator.MapData {
+	return govalidator.MapData{
+		"ride_uuid":             []string{"required", "uuid_v4"},
+		"client_uuid":           []string{"required", "uuid_v4"},
+		"origin_latitude":       []string{"required", "lat"},
+		"origin_longitude":      []string{"required", "lon"},
+		"destination_latitude":  []string{"required", "lat"},
+		"destination_longitude": []string{"required", "lon"},
+	}
+}
+
+func (r *StartTripRequest) Origin() location.Coordinate {
+	return location.Coordinate{
+		Lat: r.OriginLatitude,
+		Lon: r.OriginLongitude,
+	}
+}
+
+func (r *StartTripRequest) Destination() location.Coordinate {
+	return location.Coordinate{
+		Lat: r.DestinationLatitude,
+		Lon: r.DestinationLongitude,
+	}
+}
